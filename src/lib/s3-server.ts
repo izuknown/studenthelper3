386cc12d676c61788f5671f2e11f1024ebe1c73c
izuknown <okway_1994@hotmail.com>
2024-01-23 10:29:44 +0000

    Updated temp file name

--- src/lib/s3-server.ts
+++ src/lib/s3-server.ts
@@ -20,7 +20,7 @@ export async function downloadFromS3(file_key: string) {
         };
 
         const obj = await s3.getObject(params).promise()
-        const file_name = '/tmp/${Date.now()}'
+        const file_name = `/tmp/${Date.now()}`;
         fs.writeFileSync(file_name,obj.Body as Buffer)
         return file_name
         
