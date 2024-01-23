b2f1a8cd8efcaaf3697986343ea6ddb4db6b1063
izuknown <okway_1994@hotmail.com>
2024-01-23 12:59:00 +0000

    Updated to AWS SDK v3

--- src/lib/s3-server.ts
+++ src/lib/s3-server.ts
@@ -1,17 +1,15 @@
-import AWS from 'aws-sdk';
+import { S3 } from '@aws-sdk/client-s3';
 import fs from 'fs';
+import path from 'path';
 
 export async function downloadFromS3(file_key: string) {
     try {
-        AWS.config.update({
-            accessKeyId: process.env.NEXT_PUBLIC_S3_ACCESS_KEY_ID,
-            secretAccessKey: process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY,
-        });
-        const s3 = new AWS.S3({
-            params: {
-                Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME,
+        const s3 = new S3({
+            region: 'eu-west-2',
+            credentials: {
+                accessKeyId: process.env.NEXT_PUBLIC_S3_ACCESS_KEY_ID!,
+                secretAccessKey: process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY!,
             },
-            region: 'eu-west-2'
         });
 
         const params = {
@@ -19,13 +17,25 @@ export async function downloadFromS3(file_key: string) {
             Key: file_key
         };
 
-        const obj = await s3.getObject(params).promise()
-        const file_name = `/tmp/${Date.now()}`;
-        fs.writeFileSync(file_name,obj.Body as Buffer)
-        return file_name
-        
+        const { Body } = await s3.getObject(params);
+
+        const tmpDir = 'C:\\tmp';  // Adjust the directory as per your OS and requirements
+        const fileName = path.join(tmpDir, `${Date.now()}.txt`); // Example, you can change the extension
+
+        if (!fs.existsSync(tmpDir)) {
+            fs.mkdirSync(tmpDir);
+        }
+
+        if (Body instanceof Buffer) {
+            fs.writeFileSync(fileName, Body);
+        } else {
+            console.error('Received data is not a buffer');
+            return null;
+        }
+
+        return fileName;
     } catch (error) {
-        console.error(error)
-        return null
+        console.error('Error downloading from S3:', error);
+        return null;
     }
 }
