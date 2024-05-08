e1bf46f6e9043569562ba61dcd6e4eb5b0edecdb
izuknown <okway_1994@hotmail.com>
2024-05-08 17:41:00 +0100

    Investigate whether the process of assiging variable works as intended.

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -25,7 +25,8 @@ export async function POST(req: Request) {
     const lastMessage = messages[messages.length - 1];
 
     const context = await getContext(lastMessage.content, fileKey);
-
+    
+    console.log('Context:', context, 'context');
     const prompt = {
       role: "system",
       content: `AI assistant is brand new powerful human-like artificial intelligence.
@@ -33,14 +34,14 @@ export async function POST(req: Request) {
       AI is always well-behaved, well-mannered, and well-meaning.
       AI is always kind and friendly and utilizes the Socratic method to help guide the learning and understanding of others.
       AI is a teacher and has the sum of all knowledge in their brain and is able to accurately answer questions with answers that guide others to discover solutions.
+      START OF CONTEXT BLOCK.
+      ${context} 
+      END OF CONTEXT BLOCK.
       AI teacher will take into account any CONTEXT BLOCK provided.
       AI will use the context to answer any questions referencing the context directly and will do so utilizing a Socratic style of teaching.
       If the context does not provide an answer to the question, the AI teacher will ask the relevance of the question and will help guide the student towards discovering the answer.
-      AI will not invent anything that is not drawn directly from context...
-      START OF CONTEXT BLOCK.
-      ${context} 
-      END OF CONTEXT BLOCK.`,
-  };
+      AI will not invent anything that is not drawn directly from context`,
+    };
 
     const response = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
@@ -50,24 +51,33 @@ export async function POST(req: Request) {
       ],
       stream: true,
     });
+    console.log('context is', context)
+    console.log('prompt is', prompt)
+    console.log('Message is', messages)
+    console.log('Generating AI response using OpenAIs createChatCompletion method, chat/route.ts.')
+
 
     const stream = OpenAIStream(response, {
         onStart: async () => {
+          console.log('save user message to database, chat/rout.ts') 
           await db.insert(_messages).values({
             chatId,
             messageContent: lastMessage.content,
             role: 'user'
           });
+          console.log('inserting user message to database, chat/rout.ts')
         },
         onCompletion: async (completion) => {
+          console.log('save ai message to database, chat/rout.ts')
           await db.insert(_messages).values({
             chatId,
             messageContent: completion,
             role: 'system'
           });
+          console.log('inserting ai message to database, chat/rout.ts')
         }
       });
-
+    console.log('Returning a StreamingTextResponse with the generated response stream')
     return new StreamingTextResponse(stream);
   } catch (error){
     console.log('error in chat process', error)
